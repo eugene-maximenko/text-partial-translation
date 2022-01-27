bcdaa5cf28708afa382f661dbdacb175843f2543
ShadowPrince <zhek4770@gmail.com>
2022-01-27 23:50:12 +0200

    Implement primitive logic

--- logic.js
+++ logic.js
@@ -3,22 +3,53 @@ const englishOrigin = [
 ];
 
 const russianOrigin = [
-  'Босюсь, Ватсон, что мне придется уехать, - сказал Холмс.',
+  'Я босюсь, Ватсон, что мне придется уехать, - сказал Холмс.',
 ];
 // =>
 
 const englishSplited = [
-  '"I am',
-  'afraid,',
+  '"I',
+  'am afraid,',
   'Watson,',
   'that',
   'I',
-  'shall have to go,',
-  'said Holmes."',
+  'shall have',
+  'to go",',
+  'said',
+  'Holmes."',
 ];
 
 const russianSplited = [
-  'Босюсь, Ватсон, что мне придется уехать, - сказал Холмс.',
+  'Я',
+  'босюсь,',
+  'Ватсон,',
+  'что',
+  'мне',
+  'придется',
+  'уехать',
+  '- сказал',
+  'Холмс.',
 ];
 
-console.log(englishSplited.join(' '));
+const usedIndexes = [];
+
+function generateRandomIndex(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+
+  if (!usedIndexes.includes(randomIndex)) {
+    return randomIndex;
+  }
+
+  return generateRandomIndex(array);
+}
+
+for (let i = 0; i < englishSplited.length; i++) {
+  if (russianSplited.length !== englishSplited.length) {
+    throw new Error();
+  }
+
+  const randomIndex = generateRandomIndex(englishSplited);
+  englishSplited.splice(randomIndex, 1, russianSplited[randomIndex]);
+  console.log(englishSplited.join(' '));
+  usedIndexes.push(randomIndex);
+}
