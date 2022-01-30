2538e8d975667ceb7b9d9248cd003a003ec399eb
ShadowPrince <zhek4770@gmail.com>
2022-01-30 23:40:21 +0200

    Add current field to text model

--- adventure-1-silver-blaze-part-1.js
+++ adventure-1-silver-blaze-part-1.js
@@ -1,57 +1,71 @@
 exports.text = [
     {
+        current: '',
         english: '"I',
         russian: 'Я',
     },
     {
+        current: '',
         english: 'am afraid,',
         russian: 'босюсь,',
     },
     {
+        current: '',
         english: 'Watson,',
         russian: 'Ватсон,',
     },
     {
+        current: '',
         english: 'that',
         russian: 'что',
     },
     {
+        current: '',
         english: 'I',
         russian: 'мне',
     },
     {
+        current: '',
         english: 'shall have',
         russian: 'придется',
     },
     {
+        current: '',
         english: 'to go",',
         russian: 'уехать,',
     },
     {
+        current: '',
         english: 'said',
         russian: '- сказал',
     },
     {
+        current: '',
         english: 'Holmes,',
         russian: 'Холмс,',
     },
     {
+        current: '',
         english: 'as',
         russian: 'когда',
     },
     {
+        current: '',
         english: 'we',
         russian: 'мы',
     },
     {
+        current: '',
         english: 'sat down together',
         russian: 'сели',
     },
     {
+        current: '',
         english: 'to our breakfast',
         russian: 'позавтракать',
     },
     {
+        current: '',
         english: 'one morning.',
         russian: 'однажны утром.',
     },
