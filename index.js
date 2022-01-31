619acafc1d17cbf1a6649eae8a25589b447b4513
ShadowPrince <zhek4770@gmail.com>
2022-02-01 00:35:14 +0200

    Add parseTextInSpecificLanguage function

---
+++ index.js
@@ -0,0 +1,20 @@
+const {text} = require('./adventure-1-silver-blaze-part-1');
+const {generateRandomIndex, usedIndexes} = require("./generateRandomIndex");
+const {fillCurrentField} = require("./generateRandomIndex copy");
+const {parseTextInSpecificLanguage} = require('./src/lib/buttonsHandlers')
+fillCurrentField(text, 'english')
+
+// for (let i = 0; i <= text.length; i++) {
+//   let string = '';
+//   const randomIndex = generateRandomIndex(text);
+
+//   string = string.concat(...text.map((e) => e.current).join(' '));
+//   console.log(string, '\n');
+//   if (i < text.length) {
+//     text[randomIndex].current = text[randomIndex].russian;
+//   }
+//   usedIndexes.push(randomIndex);
+// }
+
+console.log(parseTextInSpecificLanguage(text, 'english'))
+
--- src/lib/buttonsHandlers.js
+++ src/lib/buttonsHandlers.js
@@ -1,3 +1,28 @@
-const getTextVersion = (text, language) => {
-    text.join(' ')
+const parseTextInSpecificLanguage = (text, language) => {
+
+    const textPiecesWithoutSpaces = text.map((e) => e[`${ language }`])
+
+    return textPiecesWithoutSpaces.join(" ")
 }
+
+const generateRandomIndex = (text, usedIndexes) => {
+    const randomIndex = Math.floor(Math.random() * text.length);
+
+    if (usedIndexes.length < text.length) {
+
+        if (!usedIndexes.includes(randomIndex)) {
+            return randomIndex;
+        }
+
+        return generateRandomIndex(text);
+    }
+}
+
+const translateOneMoreWord = (text, pointer,) => {
+
+}
+
+module.exports = {
+    parseTextInSpecificLanguage
+}
+
