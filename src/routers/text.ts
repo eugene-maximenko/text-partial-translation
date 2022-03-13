5b88c75e877feeb9b91e6fac1e152cb7c6ca90ed
ShadowPrince <zhek4770@gmail.com>
2022-03-13 22:10:54 +0200

    Create first router

--- src/routers/text.ts
+++ src/routers/text.ts
@@ -3,11 +3,19 @@ const express = require('express')
 const router = new express.Router()
 
 // Root route
-router.get('/s', (req: any, res: any) => {
+router.get('', (req: any, res: any) => {
 
     console.log('New connection to text page')
 
     res.render('text');
 });
+
+router.get('/text', (req: any, res: any) => {
+    console.log('Server get request for text');
+
+    res.json({
+        'text': 'Router text'
+    })
+})
 
 module.exports = router
